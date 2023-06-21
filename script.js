c676b7a0077a565b4a9b181d6b7e714d98f6f7a7
linggladys <ygladysling@gmail.com>
2023-06-21 11:47:34 +0800

    2nd commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// for the sticky navbar's background colour to return as user scrolls
 const navbar = document.querySelector('#customNav');
 window.onscroll=()=>{
     if(window.scrollY > 100){
@@ -7,6 +8,7 @@ window.onscroll=()=>{
     }
 }
 
+// countdown widget
 let countdownDate = new Date("Oct 13, 2023 15:37:25").getTime();
 let x = setInterval(function(){
     let currentDate = new Date().getTime();
@@ -18,7 +20,7 @@ let x = setInterval(function(){
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.querySelector(".countdown-holder").innerHTML = '<div class="col-md-3">' +  days + ' days </div> <div class="col-md-3"> ' + hours + ' hours </div> <div class="col-md-3"> ' + minutes + ' minutes </div> <div class="col-md-3"> ' + seconds + " seconds </div>";
+    document.querySelector(".countdown-holder").innerHTML = '<div class="col-md-3"> <span class="date-countdown">' +  days + ' </span> days  </div> <div class="col-md-3"> <span class="date-countdown">' + hours + '</span> hours </div> <div class="col-md-3"> <span class="date-countdown">' + minutes + '</span> minutes </div> <div class="col-md-3"> <span class="date-countdown">' + seconds + "</span> seconds </div>";
 
     if (distance < 0) {
         clearInterval(x);
